b86bc3f1e594c793e05c24161a344ac2e8949777
Cyrus <email": null>
2016-04-18 13:47:28 -0700

    Final upload for bp-example test app - All tests work locally

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -6,7 +6,8 @@ class VisitorsController < ApplicationController
     mailer = JSON.generate({ 'name' => mailer_params[:name],
       'email' => mailer_params[:email],
       'message' => mailer_params[:message] })
-    PostmanWorker.perform_async(mailer, 5)
+    # PostmanWorker.perform_async(mailer, 5)
+    PostmanWorker.perform_in(2.minutes, mailer, 5)
     redirect_to :root
     # render 'index'
   end
